15fe80b9bcca10be8e3479fb2b640190846b2304
Jonas Andersen <Jonas@anders1.com>
2025-04-29 16:13:35 +0200

    UPT: FIX

--- client/src/firebaseConfig.ts
+++ client/src/firebaseConfig.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 
+
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -8,6 +10,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
